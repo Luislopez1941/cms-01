20c3994194576a9ee2a53c1bec6665bb00347ee6
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2025-01-23 16:19:54 -0500

    change: url endPoint

--- src/services/configurationAPIs.tsx
+++ src/services/configurationAPIs.tsx
@@ -3,7 +3,7 @@ import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
 class ApiService {
   private axiosInstance: AxiosInstance;
   
- constructor(baseURL: string = 'https://back-ecommerce-kyxa.onrender.com/') {
+ constructor(baseURL: string = 'https://fabulous-nature-production.up.railway.app/') {
     this.axiosInstance = axios.create({
       baseURL,
       headers: {
